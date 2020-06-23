b573467e280b4b3040163b9122102bce36505236
crjcarlos1 <crjcarlos1@users.noreply.github.com>
2020-06-22 22:31:24 -0500

    Added mainFragment

--- app/src/main/java/com/cralos/introductionmvi/MainActivity.kt
+++ app/src/main/java/com/cralos/introductionmvi/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.cralos.introductionmvi
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        showMainFragment()
+    }
+
+    private fun showMainFragment() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, MainFragment(), "MainFragment")
+            .commit()
     }
 }
