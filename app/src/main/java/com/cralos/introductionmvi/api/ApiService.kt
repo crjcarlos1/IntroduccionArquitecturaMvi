807d87698b89e48765c7a04962f2a70fcedb9fdc
crjcarlos1 <crjcarlos1@users.noreply.github.com>
2020-06-25 17:28:59 -0500

    Added GenericApiResponse.kt Added LiveDataCallAdapter.kt Added LiveDataCallAdapterFactory.kt

--- app/src/main/java/com/cralos/introductionmvi/api/ApiService.kt
+++ app/src/main/java/com/cralos/introductionmvi/api/ApiService.kt
@@ -1,16 +1,18 @@
 package com.cralos.introductionmvi.api
 
+import androidx.lifecycle.LiveData
 import com.cralos.introductionmvi.model.BlogPost
 import com.cralos.introductionmvi.model.User
+import com.cralos.introductionmvi.util.GenericApiResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface ApiService {
 
     @GET("placeholder/user/{userId}")
-    fun getUser(@Path("userId") userId: String): User
+    fun getUser(@Path("userId") userId: String): LiveData<GenericApiResponse<User>>
 
     @GET("placeholder/blogs")
-    fun getBlogs(): List<BlogPost>
+    fun getBlogs(): LiveData<GenericApiResponse<List<BlogPost>>>
 
 }
--- app/src/main/java/com/cralos/introductionmvi/api/MyRetrofitBuilder.kt
+++ app/src/main/java/com/cralos/introductionmvi/api/MyRetrofitBuilder.kt
@@ -1,5 +1,6 @@
 package com.cralos.introductionmvi.api
 
+import com.cralos.introductionmvi.util.LiveDataCallAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -8,7 +9,10 @@ object MyRetrofitBuilder {
     const val BASE_URL: String = "https://open-api.xyz/"
 
     val retrofitBuilder: Retrofit.Builder by lazy {
-        Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create())
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(LiveDataCallAdapterFactory())
     }
 
     val service: ApiService by lazy {
